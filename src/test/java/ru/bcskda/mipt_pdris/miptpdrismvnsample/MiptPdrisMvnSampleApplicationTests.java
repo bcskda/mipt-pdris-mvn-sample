ce3ed26b7c5a705a918155b4fe738d624f02e5b0
bcskda <demulkup@ya.ru>
2021-12-15 14:23:00 +0300

    scaffold application

---
+++ src/test/java/ru/bcskda/mipt_pdris/miptpdrismvnsample/MiptPdrisMvnSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bcskda.mipt_pdris.miptpdrismvnsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiptPdrisMvnSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
