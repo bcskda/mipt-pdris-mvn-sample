afc3c8b4c9139e3393550dda84b750fe6090c1bd
bcskda <demulkup@ya.ru>
2021-12-15 14:31:49 +0300

    tests

--- src/test/java/ru/bcskda/mipt_pdris/miptpdrismvnsample/MiptPdrisMvnSampleApplicationTests.java
+++ src/test/java/ru/bcskda/mipt_pdris/miptpdrismvnsample/MiptPdrisMvnSampleApplicationTests.java
@@ -1,5 +1,6 @@
 package ru.bcskda.mipt_pdris.miptpdrismvnsample;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class MiptPdrisMvnSampleApplicationTests {
 
 	@Test
 	void contextLoads() {
+    Assertions.assertTrue(true);
 	}
 
 }
