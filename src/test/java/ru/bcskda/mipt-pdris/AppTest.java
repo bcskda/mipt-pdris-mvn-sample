203ec9e607c47a6a00869d9a1d8a1358afcefa26
bcskda <demulkup@ya.ru>
2021-12-15 12:19:02 +0300

    Initial commit

---
+++ src/test/java/ru/bcskda/mipt-pdris/AppTest.java
@@ -0,0 +1,20 @@
+package ru.bcskda.mipt_pdris;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( false );
+    }
+}
